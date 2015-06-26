f30b1d8df54a798fd04eb253d124914b3b59abae
Pei Ran Li <prli@uwaterloo.ca>
2015-06-26 16:49:30 -0400

    works sometimes to find pswd

--- timingAttack.cpp
+++ timingAttack.cpp
@@ -1,10 +1,15 @@
-#include <inttypes.h>
 #include <string>
+#include <algorithm>
 #include <iostream>
+#include <map>
+#include <sstream>
+
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
 using namespace std;
 
 bool password_ok (const string & pwd);
-string getPwdAttempt (const string & correctPwd);
 
 static __inline__ uint64_t rdtsc()
 {
@@ -13,34 +18,107 @@ static __inline__ uint64_t rdtsc()
 	return ((uint64_t)lo) | (((uint64_t)hi) << 32);
 }
 
+string m_pswd;
+
+struct letterStats {
+	char letter;
+	uint64_t sum;
+	uint64_t sqr;
+	double mean;
+	double variance;
+	double sd;
+	double minRange;
+	double maxRange;
+} ;
+
 int main()
 {
-	uint64_t start;
-	uint64_t end;
-	uint64_t exec_time;
-	string pwd_attempt = "";
-	string pwd_correct = "crazylongpasswor";
 	bool isSuccess = false;
+	
+	cout << "Please enter password: " << endl;
+	getline(cin, m_pswd);
+	
+	string pwd_correct = "";
 	while(!isSuccess)
 	{
-		pwd_attempt = getPwdAttempt(pwd_correct);
-		start = rdtsc();
-		isSuccess = password_ok (pwd_attempt);
-		end = rdtsc();
-		exec_time = end - start;
-		cout << exec_time << endl;
+		string N_max_string;
+		cout << "Please enter N value (larger N if there is confidence interval overlap): " << endl;
+		getline(cin, N_max_string);
+		int N_max;
+		std::istringstream(N_max_string) >> N_max;
+		char letters[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+		
+		//setup map
+		std::map<char,letterStats> charMap;
+		for(int i = 0; i < 26; i++)
+		{
+			letterStats stat;
+			stat.letter = letters[i];
+			stat.sum = 0;
+			stat.sqr = 0;
+			stat.mean = 0;
+			stat.variance = 0;
+			stat.sd = 0;
+			stat.minRange = 0;
+			stat.maxRange = 0;
+			charMap.insert ( std::pair<char,letterStats>(letters[i], stat));
+		}
+		
+		//warmup
+		std::random_shuffle(letters, letters+26);
+		for(int x = 0; x < 1000; x++)
+		{
+			for(int i = 0; i < 26; i++)
+			{
+				password_ok (pwd_correct + letters[i]);
+			}
+		}
+		
+		uint64_t start = 0;
+		uint64_t end = 0;
+		
+		for(int n = 0; n < N_max; n++)
+		{
+			std::random_shuffle(letters, letters+26);
+			for(int i = 0; i < 26; i++)
+			{
+				string psw_attempt = pwd_correct + letters[i];
+				start = rdtsc();
+				password_ok (psw_attempt);
+				end = rdtsc();
+				charMap[letters[i]].sum += end-start;
+				charMap[letters[i]].sqr += (end-start)*(end-start);
+			}
+			if(n%100000 == 0)
+			{
+				cout << ".";
+				std::cout.flush();
+			}
+			
+		}
+		cout << endl;
+		typedef map<char, letterStats>::iterator it_type;
+		for(it_type iterator = charMap.begin(); iterator != charMap.end(); iterator++) {
+			letterStats stat = iterator->second;
+			stat.mean = double(stat.sum) / N_max;
+			stat.variance = double(stat.sqr + 2*stat.mean*stat.sum + N_max*stat.mean) / (N_max-1);
+			stat.sd = sqrt(stat.variance/N_max);
+			stat.minRange = stat.mean - double(1.96*stat.sd)/sqrt(N_max);
+			stat.maxRange = stat.mean + double(1.96*stat.sd)/sqrt(N_max);
+			printf ("%c -  sum: %u minRange: %.4f maxRange: %.4f \n", stat.letter, stat.sum, stat.minRange, stat.maxRange);
+		}
+
+		cout << "Please enter probable password sequence: " << endl;
+		getline(cin, pwd_correct);
+		isSuccess = password_ok (pwd_correct);
 	}
-	cout << "OK" << endl;
+
+	cout << "OK, security breached" << endl;
     return 0;
 }
 
 bool password_ok (const string & pwd)
 {    
-	return pwd == "crazylongpassword";
+	return pwd == m_pswd;
 }
 
-string getPwdAttempt (const string & pwd_correct)
-{
-	char randChar = 'a' + (random() % 26);
-	return pwd_correct + randChar;
-}
